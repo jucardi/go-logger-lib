c48f84770211c358af194f485ee0f61e7f53e51f
Juan C. Diaz <me@jucardi.com>
2018-05-16 22:59:22 -0700

    fixed function docs for interface

--- log/logger.go
+++ log/logger.go
@@ -7,49 +7,48 @@ package log
 type ILogger interface {
 	// SetLevel sets the logging level
 	SetLevel(level Level)
-
 	// GetLevel gets the logging level
 	GetLevel() Level
 
-	// Debug logs a message at level Debug on the standard logger.
+	// Debug logs a message at level Debug on the logger.
 	Debug(args ...interface{})
-	// Debugf logs a message at level Debug on the standard logger.
+	// Debugf logs a message at level Debug on the logger.
 	Debugf(format string, args ...interface{})
-	// Debugln logs a message at level Debug on the standard logger.
+	// Debugln logs a message at level Debug on the logger.
 	Debugln(args ...interface{})
 
-	// Info logs a message at level Info on the standard logger.
+	// Info logs a message at level Info on the logger.
 	Info(args ...interface{})
-	// Infof logs a message at level Info on the standard logger.
+	// Infof logs a message at level Info on the logger.
 	Infof(format string, args ...interface{})
-	// Infoln logs a message at level Info on the standard logger.
+	// Infoln logs a message at level Info on the logger.
 	Infoln(args ...interface{})
 
-	// Warn logs a message at level Warn on the standard logger.
+	// Warn logs a message at level Warn on the logger.
 	Warn(args ...interface{})
-	// Warnf logs a message at level Warn on the standard logger.
+	// Warnf logs a message at level Warn on the logger.
 	Warnf(format string, args ...interface{})
-	// Warnln logs a message at level Warn on the standard logger.
+	// Warnln logs a message at level Warn on the logger.
 	Warnln(args ...interface{})
 
-	// Error logs a message at level Error on the standard logger.
+	// Error logs a message at level Error on the logger.
 	Error(args ...interface{})
-	// Errorf logs a message at level Error on the standard logger.
+	// Errorf logs a message at level Error on the logger.
 	Errorf(format string, args ...interface{})
-	// Errorln logs a message at level Error on the standard logger.
+	// Errorln logs a message at level Error on the logger.
 	Errorln(args ...interface{})
 
-	// Fatal logs a message at level Fatal on the standard logger.
+	// Fatal logs a message at level Fatal on the logger.
 	Fatal(args ...interface{})
-	// Fatalf logs a message at level Fatal on the standard logger.
+	// Fatalf logs a message at level Fatal on the logger.
 	Fatalf(format string, args ...interface{})
-	// Fatalln logs a message at level Fatal on the standard logger.
+	// Fatalln logs a message at level Fatal on the logger.
 	Fatalln(args ...interface{})
 
-	// Panic logs a message at level Panic on the standard logger.
+	// Panic logs a message at level Panic on the logger.
 	Panic(args ...interface{})
-	// Panicf logs a message at level Panic on the standard logger.
+	// Panicf logs a message at level Panic on the logger.
 	Panicf(format string, args ...interface{})
-	// Panicln logs a message at level Panic on the standard logger.
+	// Panicln logs a message at level Panic on the logger.
 	Panicln(args ...interface{})
 }
